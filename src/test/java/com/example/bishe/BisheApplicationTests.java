d50dca1e58ac357df201f549fd419a32012cdd44
Lian Wenyi <shermanlian@163.com>
2020-04-27 13:52:51 +0800

    创建一个spring boot的项目上传到GitHub

---
+++ src/test/java/com/example/bishe/BisheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bishe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BisheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
